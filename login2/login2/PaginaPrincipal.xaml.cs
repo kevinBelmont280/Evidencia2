cd01177167d0890cf42ab35a428fb2ac067ef717
Kevin Belmont280 <juanbell289@gmail.com>
2024-10-20 15:55:19 -0600

    Actualizacion1

---
+++ login2/login2/PaginaPrincipal.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace login2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Inicio Correcta", "Bienvenido", "salir");
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("Vamos a registarte", "Bienvenido", "salir");
+
+        }
+    }
+}
